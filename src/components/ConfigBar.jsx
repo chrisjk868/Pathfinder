745a78e539403b17fb76c3a99edc77c2adf27bf0
Christopher Ku <chrisjk868@gmail.com>
2023-11-02 14:56:13 -0700

    added functionality to select path and maze algos

--- src/components/ConfigBar.jsx
+++ src/components/ConfigBar.jsx
@@ -1,33 +1,48 @@
 import { Button, Container, Dropdown } from "react-bootstrap";
+import { useState } from "react";
 import './styles/ConfigBar.css';
 
 function ConfigBar({ runBfs, clearBoard, generateMaze, btnStates }) {
+
+    const [mazeAlgo, setMazeAlgo] = useState('Select Maze Generation Algorithm');
+    const [pfAlgo, setPfAlgo] = useState('Select Path Finding Algorithm');
 
     const getState = (btn) => {
         let states = JSON.parse(btnStates);
         return states[btn];
     }
 
+    const handleSelect = (_eventKey, event) => {
+        console.log(event.target.className);
+        event.preventDefault();
+        let target = event.target.className;
+        if (target === 'mazeAlgo dropdown-item') {
+            setMazeAlgo(event.target.text);
+        } else {
+            setPfAlgo(event.target.text);
+        }
+    }
+
     return (
         <div className="configBar">
-            <Dropdown>
+            <Dropdown onSelect={handleSelect}>
                 <Dropdown.Toggle variant="secondary">
-                    Select Maze Generation Algorithm
+                    {mazeAlgo}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item>DFS</Dropdown.Item>
-                    <Dropdown.Item>MST</Dropdown.Item>
+                    <Dropdown.Item className='mazeAlgo'>Randomized DFS</Dropdown.Item>
+                    <Dropdown.Item className='mazeAlgo'>MST</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-            <Dropdown>
+            <Dropdown onSelect={handleSelect}>
                 <Dropdown.Toggle variant="secondary">
-                    Select Path Finding Algorithm
+                    {pfAlgo}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item>BFS</Dropdown.Item>
-                    <Dropdown.Item>DFS</Dropdown.Item>
-                    <Dropdown.Item>Disjkstra's</Dropdown.Item>
-                    <Dropdown.Item>A*</Dropdown.Item>
+                    <Dropdown.Item className='pathAlgo'>BFS</Dropdown.Item>
+                    <Dropdown.Item className='pathAlgo'>DFS</Dropdown.Item>
+                    <Dropdown.Item className='pathAlgo'>Disjkstra's</Dropdown.Item>
+                    <Dropdown.Item className='pathAlgo'>A*</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
             <Button id="gen-maze" variant="outline-light" onClick={() => {generateMaze();}} disabled={getState('gen-maze')}> Create Maze </Button>
