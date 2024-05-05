623a7a6a89d101f374079a2c5a1424d3a6cc313a
Christopher Ku <chrisjk868@gmail.com>
2024-05-05 16:04:41 -0700

    Defined UF

--- src/utils/UnionFind.js
+++ src/utils/UnionFind.js
@@ -1,3 +1,16 @@
 class UnionFind {
-    
+
+    constructor(r, c) {
+        this.parent = Array(r * c).fill().map((_, idx) => idx);
+        this.rank = Array(r * c).fill(1);
+    }
+
+    find() {
+        
+    }
+
+    union() {
+
+    }
+
 }
